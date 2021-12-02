d05975ef541b31b4e0a89a21d3b13f3ef39f9728
mizuday <mizuday5.7@gmail.com>
2021-12-02 18:26:10 +0700

    Update installer

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-# A basic install script for pokemon-colorscripts
+# A basic install script for anime-colorscripts
 
 INSTALL_DIR='/usr/local/opt'
 BIN_DIR='/usr/local/bin'
 
 # deleting directory if it already exists
-rm -rf $INSTALL_DIR/pokemon-colorscripts || return 1
+rm -rf $INSTALL_DIR/anime-colorscripts || return 1
 
 # making the necessary folder structure
-mkdir -p $INSTALL_DIR/pokemon-colorscripts || return 1
+mkdir -p $INSTALL_DIR/anime-colorscripts || return 1
 
 # moving all the files to appropriate locations
-cp -rf colorscripts $INSTALL_DIR/pokemon-colorscripts
-cp pokemon-colorscripts.sh $INSTALL_DIR/pokemon-colorscripts
-cp nameslist.txt $INSTALL_DIR/pokemon-colorscripts
+cp -rf colorscripts $INSTALL_DIR/anime-colorscripts
+cp anime-colorscripts.sh $INSTALL_DIR/anime-colorscripts
+cp charalist.txt $INSTALL_DIR/anime-colorscripts
 
 # create symlink in usr/bin
-rm -rf $BIN_DIR/pokemon-colorscripts || return 1
-ln -s $INSTALL_DIR/pokemon-colorscripts/pokemon-colorscripts.sh $BIN_DIR/pokemon-colorscripts
+rm -rf $BIN_DIR/anime-colorscripts || return 1
+ln -s $INSTALL_DIR/anime-colorscripts/anime-colorscripts.sh $BIN_DIR/anime-colorscripts
 
